42ebb95c583d37cc29acfb686f4c77b4ff6d1395
kwangil.ha <kwangil.ha@musinsa.com>
2024-06-24 17:39:54 +0900

    Add http route

--- autoinstrumentation/nodejs/src/autoinstrumentation.ts
+++ autoinstrumentation/nodejs/src/autoinstrumentation.ts
@@ -8,52 +8,86 @@ import { awsEc2Detector, awsEksDetector } from '@opentelemetry/resource-detector
 import { containerDetector } from '@opentelemetry/resource-detector-container';
 import { gcpDetector } from '@opentelemetry/resource-detector-gcp';
 import { envDetector, hostDetector, osDetector, processDetector } from '@opentelemetry/resources';
-import { diag } from '@opentelemetry/api';
+import { context, diag } from '@opentelemetry/api';
+import { getRPCMetadata } from '@opentelemetry/core';
 
 import { NodeSDK } from '@opentelemetry/sdk-node';
 
+import { IncomingMessage } from 'http';
+
 function getMetricReader() {
-    switch (process.env.OTEL_METRICS_EXPORTER) {
-        case undefined:
-        case '':
-        case 'otlp':
-            diag.info('using otel metrics exporter');
-            return new PeriodicExportingMetricReader({
-                exporter: new OTLPMetricExporter(),
-            });
-        case 'prometheus':
-            diag.info('using prometheus metrics exporter');
-            return new PrometheusExporter({});
-        case 'none':
-            diag.info('disabling metrics reader');
-            return undefined;
-        default:
-            throw Error(`no valid option for OTEL_METRICS_EXPORTER: ${process.env.OTEL_METRICS_EXPORTER}`)
-    }
+  switch (process.env.OTEL_METRICS_EXPORTER) {
+    case undefined:
+    case '':
+    case 'otlp':
+      diag.info('using otel metrics exporter');
+      return new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter(),
+        exportIntervalMillis: parseInt(process.env.OTEL_METRIC_EXPORT_INTERVAL || '60000', 10)
+      });
+    case 'prometheus':
+      diag.info('using prometheus metrics exporter');
+      return new PrometheusExporter({});
+    case 'none':
+      diag.info('disabling metrics reader');
+      return undefined;
+    default:
+      throw Error(`no valid option for OTEL_METRICS_EXPORTER: ${process.env.OTEL_METRICS_EXPORTER}`);
+  }
 }
 
-const sdk = new NodeSDK({
-    autoDetectResources: true,
-    instrumentations: [getNodeAutoInstrumentations()],
-    traceExporter: new OTLPTraceExporter(),
-    metricReader: getMetricReader(),
-    resourceDetectors:
-        [
-            // Standard resource detectors.
-            containerDetector,
-            envDetector,
-            hostDetector,
-            osDetector,
-            processDetector,
+const matchPath = (path: string) => {
+  const regex = /(favicon\.ico|_next|static)/;
+  return !regex.test(path);
+}
+const pathPrefix = process.env.PATH_PREFIX || '';
 
-            // Cloud resource detectors.
-            alibabaCloudEcsDetector,
-            // Ordered AWS Resource Detectors as per:
-            // https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/main/processor/resourcedetectionprocessor/README.md#ordering
-            awsEksDetector,
-            awsEc2Detector,
-            gcpDetector,
-        ],
+const sdk = new NodeSDK({
+  autoDetectResources: true,
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-fs': {
+        enabled: false
+      },
+      '@opentelemetry/instrumentation-http': {
+        requestHook: (span, req) => {
+          if (req instanceof IncomingMessage) {
+            if (span && req.url) {
+              const url = (pathPrefix && !req.url.startsWith(pathPrefix)) ? `${pathPrefix}${req.url}` : req.url;
+              const route = matchPath(url) ? url : '';
+
+              if (route) {
+                const rpcMetadata = getRPCMetadata(context.active());
+                if (rpcMetadata) {
+                  rpcMetadata.route = route;
+                }
+                span.setAttribute('resource.name', `${req.method || 'GET'} ${route}`);
+              }
+            }
+          }
+        },
+      },
+    })
+  ],
+  traceExporter: new OTLPTraceExporter(),
+  metricReader: getMetricReader(),
+  resourceDetectors: 
+    [
+      // Standard resource detectors.
+      containerDetector,
+      envDetector,
+      hostDetector,
+      osDetector,
+      processDetector,
+
+      // Cloud resource detectors.
+      alibabaCloudEcsDetector,
+      // Ordered AWS Resource Detectors as per:
+      // https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/main/processor/resourcedetectionprocessor/README.md#ordering
+      awsEksDetector,
+      awsEc2Detector,
+      gcpDetector,
+    ],
 });
 
 sdk.start();
